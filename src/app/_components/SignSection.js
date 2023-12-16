ef431f3e35e78a325385ce6f0da985b152e077c9
zeriong <jaeryong95@gmail.com>
2023-12-16 19:38:33 +0900

    Front: 로그인 화면 구현

--- src/app/_components/SignSection.js
+++ src/app/_components/SignSection.js
@@ -1,16 +1,33 @@
 'use client';
 export default function SignSection() {
+    // Style Constants
+    const INPUT_STYLE = "border-2 border-gray-400 px-2 py-1 rounded mt-1 w-full";
+    const SIGN_BUTTON_STYLE = "w-full py-3 bg-sky-300 rounded-xl";
     const onSignInModal = () => {
         console.log('사인인!')
     }
     const onSignUpModal = () =>{
         console.log('사인업!')
     }
+
+
     return (
-        <div>
-            <div className="flex gap-2">
-                <button onClick={onSignInModal} className="px-3 py-2 bg-sky-300">SignIn</button>
-                <button onClick={onSignUpModal} className="px-3 py-2 bg-sky-300">SignUp</button>
+        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[340px] flex flex-col gap-6 p-3">
+            <h1 className="font-bold text-3xl">
+                Sign In to Your Email!
+            </h1>
+            <label>
+                <p>E-Mail</p>
+                <input placeholder="E-Mail" className={INPUT_STYLE}/>
+            </label>
+            <label>
+                <p>Password</p>
+                <input placeholder="Password" className={INPUT_STYLE}/>
+            </label>
+
+            <div className="flex gap-4 justify-around mt-4">
+                <button onClick={onSignInModal} className={SIGN_BUTTON_STYLE}>SignIn</button>
+                <button onClick={onSignUpModal} className={SIGN_BUTTON_STYLE}>SignUp</button>
             </div>
         </div>
     )
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import SignSection from '@/app/_components/SignSection';
 export default function Home() {
 
   return (
-      <div>
+      <div className="w-full h-full">
           <SignSection/>
       </div>
   )
