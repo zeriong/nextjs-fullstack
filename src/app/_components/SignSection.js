9538081b7f6f15c965ffbce9bf5a6ae82f14d976
zeriong <jaeryong95@gmail.com>
2023-12-17 23:37:21 +0900

    Front: 로그인 모달 컨트롤 기능 구현

--- src/app/_components/SignSection.js
+++ src/app/_components/SignSection.js
@@ -1,34 +1,46 @@
 'use client';
+import SignInModal from '@/app/_modals/SignInModal';
+import SignUpModal from '@/app/_modals/SignUpModal';
+import {useState} from 'react';
+
 export default function SignSection() {
     // Style Constants
     const INPUT_STYLE = "border-2 border-gray-400 px-2 py-1 rounded mt-1 w-full";
     const SIGN_BUTTON_STYLE = "w-full py-3 bg-sky-300 rounded-xl";
+
+    const [isSignInModal, setIsSignInModal] = useState(false);
     const onSignInModal = () => {
-        console.log('사인인!')
+        setIsSignInModal(true);
+        console.log('사인인~', isSignInModal)
     }
     const onSignUpModal = () =>{
-        console.log('사인업!')
+        console.log('사인업!');
     }
 
 
     return (
-        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[340px] flex flex-col gap-6 p-3">
-            <h1 className="font-bold text-3xl">
-                Sign In to Your Email!
-            </h1>
-            <label>
-                <p>E-Mail</p>
-                <input placeholder="E-Mail" className={INPUT_STYLE}/>
-            </label>
-            <label>
-                <p>Password</p>
-                <input placeholder="Password" className={INPUT_STYLE}/>
-            </label>
+        <>
+            <div
+                className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[340px] flex flex-col gap-6 p-3">
+                <h1 className="font-bold text-3xl">
+                    Sign In to Your Email!
+                </h1>
+                <label>
+                    <p>E-Mail</p>
+                    <input placeholder="E-Mail" className={INPUT_STYLE}/>
+                </label>
+                <label>
+                    <p>Password</p>
+                    <input placeholder="Password" className={INPUT_STYLE}/>
+                </label>
 
-            <div className="flex gap-4 justify-around mt-4">
-                <button onClick={onSignInModal} className={SIGN_BUTTON_STYLE}>SignIn</button>
-                <button onClick={onSignUpModal} className={SIGN_BUTTON_STYLE}>SignUp</button>
+                <div className="flex gap-4 justify-around mt-4">
+                    <button onClick={onSignInModal} className={SIGN_BUTTON_STYLE}>SignIn</button>
+                    <button onClick={onSignUpModal} className={SIGN_BUTTON_STYLE}>SignUp</button>
+                </div>
             </div>
-        </div>
+            {isSignInModal ? <SignInModal setIsSignInModal={setIsSignInModal}/> : ""}
+            <SignUpModal/>
+        </>
     )
 }
--- src/app/_modals/SignInModal.js
+++ src/app/_modals/SignInModal.js
@@ -1,13 +1,17 @@
-export default function SignInModal() {
+'use client';
+
+export default function SignInModal({ setIsSignInModal }) {
     const signIn = (e) => {
         e.preventDefault();
-        console.log('로그인!');
     };
 
     return (
-        <form onSubmit={signIn}>
-            <input type="text" placeholder="E-Mail"/>
-            <input type="password" placeholder="Password"/>
-        </form>
+        <>
+            <div className="opacity-50 bg-black fixed w-full h-full z-10" onClick={() => setIsSignInModal(false)}/>
+            <form onSubmit={signIn} className="fixed flex flex-col z-20 bg-white left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 p-4">
+                <input type="text" placeholder="E-Mail"/>
+                <input type="password" placeholder="Password"/>
+            </form>
+        </>
     )
 }
